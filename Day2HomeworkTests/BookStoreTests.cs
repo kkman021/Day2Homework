e3614d48abc7c2fdd6fd4a8b706af0d4e82d755a
楊明勳 Ming <ming@miniasp.com>
2016-03-14 21:01:17 +0800

    初始化版本

---
+++ Day2HomeworkTests/BookStoreTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Day2Homework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Day2Homework.Tests
+{
+    [TestClass()]
+    public class BookStoreTests
+    {
+        
+    }
+}
