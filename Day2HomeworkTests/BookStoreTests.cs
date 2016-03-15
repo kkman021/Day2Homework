a5d165e6cf4bbf2585716b63cd17002b116bd2db
楊明勳 Ming <ming@miniasp.com>
2016-03-15 22:25:57 +0800

    12233集 紅燈

--- Day2HomeworkTests/BookStoreTests.cs
+++ Day2HomeworkTests/BookStoreTests.cs
@@ -117,5 +117,26 @@ namespace Day2Homework.Tests
 
             Assert.AreEqual(expectPrice, actual);
         }
+
+        [TestMethod()]
+        public void CountSumSellPrice_buy_no1_1_no2_2_no3_2_shoudl_price_460()
+        {
+            var buyList = new List<Book>
+            {
+                new Book() { BookId = 1, BookName = "哈利波特第一集", Price = 100 },
+                new Book() { BookId = 2, BookName = "哈利波特第二集", Price = 100 },
+                new Book() { BookId = 2, BookName = "哈利波特第二集", Price = 100 },
+                new Book() { BookId = 3, BookName = "哈利波特第三集", Price = 100 },
+                new Book() { BookId = 3, BookName = "哈利波特第三集", Price = 100 }
+            };
+
+            var expectPrice = 460;
+
+            var target = new BookStore();
+
+            var actual = target.CountSum(buyList);
+
+            Assert.AreEqual(expectPrice, actual);
+        }
     }
 }
