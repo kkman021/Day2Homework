75f22ccad908fa8515d302ccf5805df5a162941c
楊明勳 Ming <ming@miniasp.com>
2016-03-16 20:43:48 +0800

    12233集 綠燈

--- Day2Homework/BookStore.cs
+++ Day2Homework/BookStore.cs
@@ -11,46 +11,53 @@ namespace Day2Homework
 
         public double CountSum(List<Book> buyList)
         {
-            buyList = buyList.OrderBy(x => x.BookId).ToList();
-            var differentBookCount = 0;
-            double discount = 1;
             double totalPrice = 0;
-            int? tempId = null;
 
-            foreach (var book in buyList)
+            buyList = buyList.OrderBy(x => x.BookId).ToList();
+
+            var source = new List<Book>(buyList);
+
+            var bookGroup = new List<List<Book>>();
+
+            while (bookGroup.Sum(l => l.Distinct().Count()) < source.Count)
             {
-                if (book.BookId != tempId)
+                var list = new List<Book>();
+                int? tempId = null;
+
+                foreach (var book in buyList.ToArray())
                 {
-                    differentBookCount++;
-                }
-                else
-                {
+                    if (tempId != book.BookId)
+                    {
+                        list.Add(book);
+                        buyList.Remove(book);
+                    }
                     tempId = book.BookId;
                 }
-                tempId = book.BookId;
-
+                bookGroup.Add(list);
             }
 
-            if (differentBookCount == 2)
+
+            foreach (var group in bookGroup)
+            {
+                totalPrice = totalPrice + group.Sum(x => x.Price) * GetDisCount(group.Count);
+            }
+            return totalPrice;
+        }
+
+        private static double GetDisCount(int bookCount)
+        {
+            double discount = 1;
+
+            if (bookCount == 2)
                 discount = 0.95;
-            else if (differentBookCount == 3)
+            else if (bookCount == 3)
                 discount = 0.9;
-            else if (differentBookCount == 4)
+            else if (bookCount == 4)
                 discount = 0.8;
-            else if (differentBookCount == 5)
+            else if (bookCount == 5)
                 discount = 0.75;
 
-            for (int i = 0; i < buyList.Count; i++)
-            {
-                if (i <= differentBookCount - 1)
-                {
-                    totalPrice = totalPrice + buyList[i].Price * discount;
-                }
-                else
-                    totalPrice = totalPrice + buyList[i].Price;
-            }
-
-            return totalPrice;
+            return discount;
         }
     }
 }
